72d553518915376e54f9ee3f6b3bd2a3b1459ad1
mcostanetworks <mcostanetworks@gmail.com>
2022-01-14 16:55:21 -0300

    Capturar elementos HTML manipulaveis.

---
+++ script.js
@@ -0,0 +1,5 @@
+const imagem = document.querySelector('img'); //Cria bloco para armazenar tag img do HTML.
+const botao = document.querySelector('button');
+const nomeDoPersonagem = document.querySelector('#nome');
+const especie = document.querySelector('#especie');
+const status = document.querySelector('#status');
