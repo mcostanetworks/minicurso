bbb1317ebaa038be6b10d74f9d376dc4e6b54b28
mcostanetworks <mcostanetworks@gmail.com>
2022-01-15 17:04:41 -0300

    Ajustar IDs do nome, especie e status dos personagens de Rick and Morty.

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ const botao = document.querySelector('button');
 const nomeDoPersonagem = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
 const status = document.querySelector('#status');
+
+pegarPersoangem = () => { //Arrow function
+    return fetch(`https://rickandmortyapi.com/api/character/2`, {
+        method:'GET',
+        headers: {
+            Accept:'application/json',
+            "Content-type": 'application/json'
+        } 
+    }).then((response) => response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem.alt = data.name;
+    }); //A função then está ligada a fetch
+}
+
+botao.onclick = pegarPersonagem;
