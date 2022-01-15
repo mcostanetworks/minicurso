1d5e6f804817deefd96438fe9dde115dd8cc64c7
mcostanetworks <mcostanetworks@gmail.com>
2022-01-15 17:39:57 -0300

    Corrigir ponto e virgula.

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-const imagem = document.querySelector('img'); 
+const imagem = document.querySelector('img');
 const botao = document.querySelector('button');
 const nomeDoPersonagem = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
-const status = document.querySelector('#status');
+const condicao = document.querySelector('#status');
 
 traduzirCondicao = (data) => {
     if(data.status == 'unknown'){
         return 'Não sabemos';
     }else if(data.status == 'Alive'){
         return 'Sim';
-    }else{
-        return 'Não. Está morto.';
+    }else {
+        return 'Não. Está morto';
     }
 }
 
-gerarValorAleatorio () => {
+gerarValorAletorio = () => {
     return Math.floor(Math.random() * 671);
 }
 
 pegarPersonagem = () => {
-    let numeroAleatorio = gerarValorAleatorio();
+    let numeroAleatorio = gerarValorAletorio();
     return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
-        method: 'GET',
+        method:'GET',
         headers: {
             Accept: 'application/json',
             "Content-type": 'application/json'
