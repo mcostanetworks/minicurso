217b11a02cf5a2f2141d054e48f73e75e8f6712a
mcostanetworks <mcostanetworks@gmail.com>
2022-01-15 17:24:03 -0300

    Ajustar funcionamento do botão e da API.

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
-const imagem = document.querySelector('img'); //Cria bloco para armazenar tag img do HTML.
+const imagem = document.querySelector('img'); 
 const botao = document.querySelector('button');
 const nomeDoPersonagem = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
 const status = document.querySelector('#status');
 
-pegarPersoangem = () => { //Arrow function
-    return fetch(`https://rickandmortyapi.com/api/character/2`, {
-        method:'GET',
+traduzirCondicao = (data) => {
+    if(data.status == 'unknown'){
+        return 'Não sabemos';
+    }else if(data.status == 'Alive'){
+        return 'Sim';
+    }else{
+        return 'Não. Está morto.';
+    }
+}
+
+gerarValorAleatorio () => {
+    return Math.floor(Math.random() * 671);
+}
+
+pegarPersonagem = () => {
+    let numeroAleatorio = gerarValorAleatorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+        method: 'GET',
         headers: {
-            Accept:'application/json',
+            Accept: 'application/json',
             "Content-type": 'application/json'
-        } 
+        }
     }).then((response) => response.json()).then((data) => {
         imagem.src = data.image;
         imagem.alt = data.name;
-    }); //A função then está ligada a fetch
+        nomeDoPersonagem.innerHTML = data.name;
+        especie.innerHTML = data.species;
+        condicao.innerHTML = traduzirCondicao(data);
+    });
 }
 
 botao.onclick = pegarPersonagem;
