8f286352c56e4061892e53dc0876b2a44a3fa197
mcostanetworks <mcostanetworks@gmail.com>
2022-01-15 23:09:39 -0300

    Correr três vezes a função pegarPersonagem.

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-const imagem = document.querySelector('img');
-const botao = document.querySelector('button');
-const nomeDoPersonagem = document.querySelector('#nome');
-const especie = document.querySelector('#especie');
-const condicao = document.querySelector('#status');
+const imagem1 = document.querySelector('#img1');
+const nomeDoPersonagem1 = document.querySelector('#nome1');
+const especie1 = document.querySelector('#especie1');
+const condicao1 = document.querySelector('#condicao1');
+const imagem2 = document.querySelector('#img2');
+const nomeDoPersonagem2 = document.querySelector('#nome2');
+const especie2 = document.querySelector('#especie2');
+const condicao2 = document.querySelector('#condicao2');
+const imagem3 = document.querySelector('#img3');
+const nomeDoPersonagem3 = document.querySelector('#nome3');
+const especie3 = document.querySelector('#especie3');
+const condicao3 = document.querySelector('#condicao3');
+const botao = document.querySelector('#bilbo');
+
+document.getElementById('bilbo').addEventListener('click', function(){
+    pegarPersonagem();
+    pegarPersonagem2();
+    pegarPersonagem3();
+})
 
 traduzirCondicao = (data) => {
     if(data.status == 'unknown'){
@@ -15,7 +29,7 @@ traduzirCondicao = (data) => {
 }
 
 gerarValorAletorio = () => {
-    return Math.floor(Math.random() * 671);
+    return Math.floor(Math.random() * 826);
 }
 
 pegarPersonagem = () => {
@@ -27,11 +41,45 @@ pegarPersonagem = () => {
             "Content-type": 'application/json'
         }
     }).then((response) => response.json()).then((data) => {
-        imagem.src = data.image;
-        imagem.alt = data.name;
-        nomeDoPersonagem.innerHTML = data.name;
-        especie.innerHTML = data.species;
-        condicao.innerHTML = traduzirCondicao(data);
+        imagem1.src = data.image;
+        imagem1.alt = data.name;
+        nomeDoPersonagem1.innerHTML = data.name;
+        especie1.innerHTML = data.species;
+        condicao1.innerHTML = traduzirCondicao(data);
+    });
+}
+
+pegarPersonagem2 = () => {
+    let numeroAleatorio = gerarValorAletorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem2.src = data.image;
+        imagem2.alt = data.name;
+        nomeDoPersonagem2.innerHTML = data.name;
+        especie2.innerHTML = data.species;
+        condicao2.innerHTML = traduzirCondicao(data);
+    });
+}
+
+pegarPersonagem3 = () => {
+    let numeroAleatorio = gerarValorAletorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem3.src = data.image;
+        imagem3.alt = data.name;
+        nomeDoPersonagem3.innerHTML = data.name;
+        especie3.innerHTML = data.species;
+        condicao3.innerHTML = traduzirCondicao(data);
     });
 }
 
